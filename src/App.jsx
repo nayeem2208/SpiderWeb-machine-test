a085540cb37211c52ab9b41b4074925e02eeeb89
Nayeem <nayeem2281998@gmail.com>
2024-03-05 16:04:33 +0530

    first commit after setting up the base

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-900 w-full h-full">
+      <div
+        className="w-full h-full"
+        style={{
+          background:
+            " radial-gradient(circle, rgba(139,213,224,0.7) 0%, rgba(159,111,246,0.7133053050321692) 14%, rgba(34,34,34,0.7) 100%)",
+        }}
+      >
+        Hlleo
+      </div>
+    </div>
+  );
+}
+
+export default App;
